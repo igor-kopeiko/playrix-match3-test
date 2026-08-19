acb0f1b7f7df1e5676ee64e6af6c2f3b8e59856d
igor-kopeiko <150355332+igor-kopeiko@users.noreply.github.com>
2026-08-19 20:11:45 +0300

    Добавили состояния приложения

--- src/app/App.cpp
+++ src/app/App.cpp
@@ -3,13 +3,29 @@
 
 
 namespace match3 {
-App::App(int kBoardOffsetX, int kBoardOffsetY, int kCellSize, int space_near_cell, std::size_t cell_amount_x, std::size_t cell_amount_y) :
-	game_{ kBoardOffsetX, kBoardOffsetY, kCellSize, space_near_cell, cell_amount_x, cell_amount_y } {
-
+App::App(int kBoardOffsetX, int kBoardOffsetY, int kCellSize, int space_near_cell, std::size_t cell_amount_x, std::size_t cell_amount_y) 
+{
+	game_ = std::make_unique<Game>(kBoardOffsetX, kBoardOffsetY, kCellSize, space_near_cell, cell_amount_x, cell_amount_y);
 }
 
 void App::tick() {
-	game_.tick();
+	switch (state_) {
+	case AppState::LevelSelect: {
+		break;
+	}
+		
+	case AppState::Playing: {
+		game_->tick();
+		break;
+	}
+	}
+	
+}
+
+void App::level_select_tick() {
+	//проверить наличие файлов с уровнями
+	
+	//отрисовка меню
 }
 
 
--- src/app/App.h
+++ src/app/App.h
@@ -2,17 +2,28 @@
 #include "app/Game.h"
 
 namespace match3 {
+
+enum class AppState {
+	LevelSelect,
+	Playing
+};
+
+
+
+
 class App {
 public:
 	App(int kBoardOffsetX, int kBoardOffsetY, int kCellSize, int space_near_cell, std::size_t cell_amount_x, std::size_t cell_amount_y);
 
+	void tick();
+	
+	//void load_congigs();
+	//void level_select_tick();
 
 	
-
-	void tick();
 private:
-	Game game_;
-
+	std::unique_ptr<Game> game_;
+	AppState state_ = AppState::LevelSelect;
 
 
 
